5c489ef9c719614f972ac7c4d434e3e0a9a9cf5f
Gerardo <gerardo_orellana1996@hotmail.es>
2018-01-22 20:17:54 -0300

    agrego carpeta borrada y elimino doc erroneo

---
+++ modelos/modelocategoria.php
@@ -0,0 +1,43 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require_once '../config/config.php';
+class Modelocategoria{
+
+	private $pdo;
+
+	public function __CONSTRUCT() {
+		try{
+			$this->pdo=new PDO('mysql:host='.HOST.';dbname='.DB,USERDB,PASSDB);
+			$this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		}catch(Exception $e){
+			die($e->getMessage());
+		}
+	}
+
+	public function listar(){
+		$responsearray = array();
+		try{
+			$result = array();
+			$stm=$this->pdo->prepare("SELECT * FROM categoria");
+			$stm->execute();
+			foreach($stm->fetchALL(PDO::FETCH_OBJ) as $r){
+				$cate = new Modelocat();
+					$cate->__SET('cat_id', $r->categoria_id);
+					$cate->__SET('cat_nombre', $r->categoria_nombre);
+				$result[] = $cate->returnArray();
+			}
+			$responsearray['success']=true;
+			$responsearray['message']='Listado correctamente';
+			$responsearray['datos']=$result;
+
+		}catch(Exception $e){
+			echo $e;
+			$responsearray['success']=false;
+			$responsearray['message']='Error al listar';
+			echo "Error en la conexion con la base de datos.";
+		}
+		return $responsearray;
+	}
+}
+?>
